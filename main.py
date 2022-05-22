6c4fab18b67c0cb4840612907368b0b0b0dd8dd0
un1ck-andy <105160048+un1ck-andy@users.noreply.github.com>
2022-05-22 23:17:35 +0300

    added pdf2txt function, join and replace

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ from pathlib import Path
 
 def pdf_2_mp3(file_path='test.pdf', language='en'):
     if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
-        return 'File exists'
+        # return 'File exists'
+        with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:
+            pages = [page.extract_text() for page in pdf.pages]
+
+        #just testing how it looks before join
+        # text = str(pages)
+
+        # with open('text_before_join.txt', 'w') as file:
+        #      file.write(text)
+
+        text = ''.join(pages)
+
+        with open('text_after_join.txt', 'w') as file:
+            file.write(text)
+
+        text = text.replace('\n', '')
+
+        with open('text_after_replace.txt', 'w') as file:
+            file.write(text)
     else:
         return 'Missing file, check'
 
